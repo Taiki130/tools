642889d3b0f57f7d167f15436c8bd9d7bf87463c
Taiki130 <air19990130@yahoo.co.jp>
2023-10-09 17:09:35 +0900

    create ls

---
+++ cmd/ls.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// lsCmd represents the ls command
+var lsCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "ls – list directory contents",
+	Long:  `For each operand that names a file of a type other than directory, ls displays its name as well as any requested, associated information.  For each operand that names a file of type directory, ls displays the names of files contained within that directory, as well as any requested, associated information. If no operands are given, the contents of the current directory are displayed.  If more than one operand is given, non-directory operands are displayed first; directory and non-directory operands are sorted separately and in lexicographical order.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// fmt.Println("ls called")
+		directly := "."
+		if len(args) == 1 {
+			directly = args[0]
+		} else if len(args) > 1 {
+			log.Fatal("Too many arguments")
+		}
+
+		files, err := ioutil.ReadDir(directly)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			fmt.Println(file.Name())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lsCmd)
+}
